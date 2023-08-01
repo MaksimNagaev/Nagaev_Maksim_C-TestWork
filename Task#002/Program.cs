73aecb5c57b273016ef50ed226c91d75cfe4856c
maksimNagaev <Unforegiven@ya.ru>
2023-08-01 19:31:16 +0400

    secondTask

---
+++ Task#002/Program.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("Введите Имя");
+string x = Convert.ToString(Console.ReadLine());
+
+if (x == "Вячеслав") Console.WriteLine("Привет, Вячеслав");
+else Console.WriteLine("Нет такого имени");
