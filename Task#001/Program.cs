0f3278064087be74fb02cb57e2f15facb5febd6a
maksimNagaev <Unforegiven@ya.ru>
2023-08-01 18:58:06 +0400

    firstTask_2

--- Task#001/Program.cs
+++ Task#001/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine("Введите число");
-int x = Convert.ToInt32(Console.ReadLine());
+﻿try
+{
+    Console.WriteLine("Введите число");
+    int x = Convert.ToInt32(Console.ReadLine());
 
-if (x > 7) Console.WriteLine("Привет");
+    if (x > 7) Console.WriteLine("Привет");
+}
+
+catch
+{
+    Console.WriteLine("Вы ввели неверные данные");
+}
